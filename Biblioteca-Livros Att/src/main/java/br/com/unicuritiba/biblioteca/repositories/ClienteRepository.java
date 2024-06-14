aa3af9cce6d80d00225176ed24abc689b6ba7e1c
Paulo Vinícius Costa Rosa <105546915+paulo-vinicius318@users.noreply.github.com>
2024-06-13 22:43:25 -0300

    adicionado o trabalho A3 para o git

---
+++ Biblioteca-Livros Att/src/main/java/br/com/unicuritiba/biblioteca/repositories/ClienteRepository.java
@@ -0,0 +1,10 @@
+package br.com.unicuritiba.biblioteca.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.unicuritiba.biblioteca.models.Cliente;
+
+public interface ClienteRepository extends 
+   	JpaRepository<Cliente, Long>{
+
+}
